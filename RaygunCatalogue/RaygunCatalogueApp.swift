b0d1e1801639437f5082b9dcc2ab789d3be60339
Nik Wakelin <me@nikwakelin.com>
2024-05-28 15:07:21 +1200

    Initial Commit

---
+++ RaygunCatalogue/RaygunCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaygunCatalogueApp.swift
+//  RaygunCatalogue
+//
+//  Created by Nik Wakelin on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RaygunCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
